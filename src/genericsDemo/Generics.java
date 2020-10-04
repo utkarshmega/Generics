428a3d391bf9f19f910a92e6b691b4b1094208c1
utkarshmega <utkarsheduc@gmail.com>
2020-10-04 12:21:23 +0530

    UC2 done

--- src/genericsDemo/Generics.java
+++ src/genericsDemo/Generics.java
@@ -12,6 +12,15 @@ public class Generics {
 		}
 		System.out.println(max);
 	}
+	public static void floatMax(Float[] inpArray)
+	{
+		Float max = inpArray[0];
+		for(Float element: inpArray)
+			if(element.compareTo(max)>0)
+				max = element;
+		
+		System.out.println(max);
+	}
 
 	public static void main(String[] args) {
 				
@@ -21,6 +30,12 @@ public class Generics {
 		findMax(intArray1);
 		Integer intArray2[] = {25, 10, 50};
 		findMax(intArray2);
+		Float floatArray1[] = {10.30f, 8.30f, 4.12f};
+		floatMax(floatArray1);
+		Float floatArray2[] = {10.30f, 18.30f, 4.12f};
+		floatMax(floatArray2);
+		Float floatArray3[] = {10.30f, 8.30f, 24.12f};
+		floatMax(floatArray3);
 	}
 
 }
