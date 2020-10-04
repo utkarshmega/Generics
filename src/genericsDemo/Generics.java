8cbb251d2c8ce58177bc822614fc2f4299e48bbd
utkarshmega <utkarsheduc@gmail.com>
2020-10-04 23:28:19 +0530

    UC5 done

--- src/genericsDemo/Generics.java
+++ src/genericsDemo/Generics.java
@@ -13,14 +13,19 @@ public class Generics <T extends Comparable<T>> {
 //		return Generics.findMax(a, b, c);
 //	}
 	
-	public static <T extends Comparable<T>> void findMax(T a, T b, T c)
+	public static <T extends Comparable<T>> T findMax(T a, T b, T c)
 	{
 		T max = a;
 		if(b.compareTo(max)>0)
 			max = b;
 		if(c.compareTo(max)>0)
 			max = c;
-		System.out.println(max);
+		printMax(a, b, c, max);
+		return max;
+	}
+	public static <T> void printMax(T a, T b, T c, T max)
+	{
+		System.out.printf("Maximum of %s, %s and %s is %s\n",a, b, c, max);
 	}
 	public static void main(String[] args) {
 				
