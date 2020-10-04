dae7aeeaf65a8ab5d61bf39e22643e8686b8cfd3
utkarshmega <utkarsheduc@gmail.com>
2020-10-04 12:18:40 +0530

    UC1 done

--- src/genericsDemo/Generics.java
+++ src/genericsDemo/Generics.java
@@ -1,9 +1,26 @@
 package genericsDemo;
 
 public class Generics {
+	
+	public static void findMax(Integer[] intArray)
+	{
+		Integer max = intArray[0];
+		for(Integer element: intArray)
+		{
+			if(element.compareTo(max)>0)
+				max = element;
+		}
+		System.out.println(max);
+	}
+
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to generics demo");
+				
+		Integer intArray[] = {25, 10, 13};
+		findMax(intArray);
+		Integer intArray1[] = {25, 35, 13};
+		findMax(intArray1);
+		Integer intArray2[] = {25, 10, 50};
+		findMax(intArray2);
 	}
 
 }
